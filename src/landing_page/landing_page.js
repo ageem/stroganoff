100b728d3924eceb46b9048a680e7ed3724c00dc
Alf-13 <alfreisinger@gmail.com>
2025-03-08 10:00:07 -0600

    Updated CSS

--- src/landing_page/landing_page.js
+++ src/landing_page/landing_page.js
@@ -4,7 +4,6 @@ import StroganuffCow from './Stroganuff_cow1.png';
 function LandingPage() {
     return (
         <div className="landing_page">
-            <h1>Stroganoff!</h1>
             <img src={StroganuffCow} alt="Stroganuff Cow" />
         </div>
     );
